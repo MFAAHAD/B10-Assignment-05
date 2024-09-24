f7d3d01c3cf3233e4a9d2b2a77b14cb0b30de384
MFAAHAD <fahadmusyl@gmail.com>
2024-09-25 00:09:42 +0600

    added model.html,js and history with some edits

---
+++ JavaScript/history.js
@@ -0,0 +1,38 @@
+
+const history = document.getElementById('history').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    document.getElementById('history').classList.add('bg-lime-400', 'px-5', 'py-2', 'rounded-lg',
+        'font-bold', 'text-black', 'text-xl')
+
+    document.getElementById('donation').classList.remove('bg-lime-400', 'px-5', 'py-2', 'rounded-lg',
+        'font-bold', 'text-black', 'text-xl')
+
+    document.getElementById('donation').classList.add('border-2', 'px-5', 'py-2', 'rounded-lg')
+
+
+    document.getElementById('input').classList.add('hidden')
+    document.getElementById ('history-section').classList.remove('hidden')
+
+})
+
+const donation = document.getElementById('donation')
+donation.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    document.getElementById('input').classList.remove('hidden')
+
+    document.getElementById('donation').classList.add('bg-lime-400', 'px-5', 'py-2', 'rounded-lg',
+        'font-bold', 'text-black', 'text-xl')
+
+    document.getElementById('history').classList.remove('bg-lime-400', 'px-5', 'py-2', 'rounded-lg',
+        'font-bold', 'text-black', 'text-xl')
+
+    document.getElementById('history').classList.add('border-2', 'px-5', 'py-2', 'rounded-lg')
+    document.getElementById ('history-section').classList.add('hidden')
+
+
+
+})
+
+
